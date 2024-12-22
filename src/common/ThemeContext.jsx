9d00a0f03e12cf6df37abf2bf763f03fd942ab5f
Wilsdawg1212 <145064244+Wilsdawg1212@users.noreply.github.com>
2024-12-22 14:26:57 -0500

    Added new svg for Instagram
    t
    
    Added instagram svg dark

---
+++ src/common/ThemeContext.jsx
@@ -0,0 +1,20 @@
+import React, {createContext, useContext, useEffct, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState(
+        () => localStorage.getItem('theme') || 'light'
+    );
+
+    useEffct(() => {
+        document.body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme])
+
+    toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme == 'light' ? 'dark' : 'light'));
+    }
+};
