5ae8ef86d571fcce096cfeaff9840ab72f2d153c
Wilsdawg1212 <145064244+Wilsdawg1212@users.noreply.github.com>
2024-12-23 15:49:03 -0500

    Added Skills

---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,33 @@
+import styles from "./SkillsStyles.module.css";
+import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import SkillList from "../../common/SkillList";
+
+function Skills() {
+  return (
+    <section id="Skills" className={styles.container}>
+      <h1>Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML"/>
+        <SkillList src={checkMarkIcon} skill="CSS"/>
+        <SkillList src={checkMarkIcon} skill="JavaScript"/>
+        <SkillList src={checkMarkIcon} skill="Node"/>
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Java"/>
+        <SkillList src={checkMarkIcon} skill="Python"/>
+        <SkillList src={checkMarkIcon} skill="Rust"/>
+        <SkillList src={checkMarkIcon} skill="C"/>
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Git"/>
+        <SkillList src={checkMarkIcon} skill="PyTorch"/>
+        <SkillList src={checkMarkIcon} skill="Django"/>
+        <SkillList src={checkMarkIcon} skill="React"/>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
