4afffaa78c9030a84042a9b0f212de6587025f8f
Wilsdawg1212 <145064244+Wilsdawg1212@users.noreply.github.com>
2025-09-19 02:58:22 -0400

    Footer change

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Wilson Ide. <br />
+        &copy; 2025 Wilson Ide. <br />
         All rights reserved.
       </p>
     </section>
